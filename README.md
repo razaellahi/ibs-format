c645cfc2c36b81540733eb49ee8206d9992c23e1
raza.ellahi <raza.ellahi@expertflow.com>
2020-07-17 15:00:20 +0500

    add generic support

--- README.md
+++ README.md
@@ -10,35 +10,48 @@ Format the text into bold, italic and strike by applying HTML tags.
 
 `npm i ibs-format --save`
 
-Then...
+# Usage
 
 ```
 import { ibsFormat } from 'ibs-format';
 
-var myText = ibsFormat("Apply *bold*, _italic_, and ~strike~ on the text");
+// The function 'ibsFormat' needs two arguments.
+// 1) the text with identifiers in the first argument, in the form of string.
+// 2) tags and identifiers in the second argument, in the form of string array.
 
+var myText = "Once upon a time, there was a *thristy* ~_crow_~."
+
+// In the array, the tag symbols in the first index and their identifier in the second index.
+
+var tagArray = [['b','*'],['i','_'],['strike','~']];
+
+// Here symbol, 'b' is using for 'bold', 'i' for 'italic' and 'strike' for 'strike' tag.
+// The user can use as many tags and their identifiers of his own choice.
+// Now the function will look like.
+
+var myText = ibsFormat(myText, tagArray);
+
+// The function will return the result with tags.
+
+Once upon a time, there was a <b>thristy</b> <strike><i>crow</i></strike>.
+
+```
+
+HTML...
+
+```
 <p [innerHTML]="myText"></p>
 
 ```
 
-## Options
-
-Use symbols before and after the character or word for applying HTML tags in the text.
-
-* asterisk symbol '*' for bold.
-* tilde symbol '~' for strike.
-* underscore symbol '_' for italic.
-
-LIKE:
-```
-Once upon a time, there was a *thristy* ~_crow_~.
-
-```
-AND THE OUTPUT WILL BE:
-```
-Once upon a time, there was a <b>thristy</b> <strike><i>crow</i></strike>.
-
-```
-RESULT:
+The result will...
 
 Once upon a time, there was a <b>thristy</b> <strike><i>crow</i></strike>.
+
+
+## Precautions
+
+* Don't change the index positioning.
+* The function does not supports double or multiple identifiers rather than double asterisks '**'.
+* Don't use same identifiers for multiple tags.
+
--- index.js
+++ index.js
@@ -1,269 +1,264 @@
- function ibsFormat(value) {
+function ibsFormat(value, arr) {
 
-    if (value != "" && value != null && value != undefined) {
-      value = intoBold(value);
-      value = intoTalic(value);
-      value = intoStrike(value);
-      return value;
+  if (value != "" && value != null && value != undefined && arr.length > 0) {
+    let counter = 0;
+    let space = "";
+    if (arr[0].constructor === Array) {
+      arr.map(e => {
+        if (counter == 0) {
+          space = " &nbsp";
+        } else {
+          space = "";
+        }
+        e[2] = (e[0].length + 1).toString();
+        if (e[1] == "*") {
+          value = astericHandler(value, e[0], e[1], parseInt(e[2]), space);
+          console.log("upper songle")
+        } else if (e[1] == '**') {
+          value = doubleAstericHandler(value, e[0], e[1], parseInt(e[2]), space);
+          console.log("upper double")
+        } else {
+          value = getFormat(value, e[0], e[1], parseInt(e[2]), space);
+        }
+        ++counter;
+      });
     } else {
-      return null;
-    }
-  
-  }
-  
-  function intoBold(text) {
-    let box = [];
-    let arr = [];
-    let finalText = "";
-  
-    let a = text.split(" ");
-  
-    let singleStericCounter = 0;
-  
-    a.forEach(e => {
-      if (
-        e.length > 1 &&
-        e.includes("*") &&
-        (e.match(/\x2a/g) || []).length == 1
-      ) {
-        ++singleStericCounter;
-      }
-    });
-    singleStericCounter = singleStericCounter - 1;
-  
-    let flag = "1";
-    let loopCounter = 0;
-    a.forEach(e => {
-      if (
-        e.length > 1 &&
-        e.includes("*") &&
-        (e.match(/\x2a/g) || []).length == 1
-      ) {
-        if (loopCounter == singleStericCounter && loopCounter % 2 == 0) {
-        } else {
-          if (flag == "1") {
-            e = e.replace(/\x2a/g, "<b>");
-            flag = "2";
-          } else {
-            e = e.replace(/\x2a/g, "</b>");
-            flag = "1";
-          }
-          ++loopCounter;
-        }
-      }
-  
-      if (e.length > 1 && (e.match(/\x2a/g) || []).length > 1) {
-        const n = (e.match(/\x2a/g) || []).length;
-  
-        for (let i = 0; i < n; i++) {
-          if (i == 0) {
-            box.push(e.indexOf("*", i));
-          } else {
-            let len = box.length - 1;
-            let v = box[len];
-            v = v + 1;
-            box.push(e.indexOf("*", v));
-          }
-        }
-        let firstIndex = box[0];
-        let lastIndex = box[box.length - 1];
-  
-        e = replaceChar(e, "<b>", firstIndex);
-        e = replaceChar(e, "</b>", lastIndex + 2);
-  
-        box = [];
-      }
-  
-      arr.push(e);
-    });
-  
-    arr.forEach(e => {
-      if (e == "") {
-        finalText = finalText + " &nbsp";
+      arr[2] = (arr[0].length + 1).toString();
+      if (arr[1] == "*") {
+        value = astericHandler(value, arr[0], arr[1], parseInt(arr[2]), " &nbsp");
+        console.log("lower songle")
+      } else if (arr[1] == '**') {
+        console.log("lower double")
+        value = doubleAstericHandler(value, arr[0], arr[1], parseInt(arr[2]), " &nbsp");
       } else {
-        finalText = finalText + e + " &nbsp";
+        value = getFormat(value, arr[0], arr[1], parseInt(arr[2]), " &nbsp");
       }
-    });
-  
-    function replaceChar(origString, replaceChar, index) {
-      let firstPart = origString.substr(0, index);
-      let lastPart = origString.substr(index + 1);
-  
-      let newString = firstPart + replaceChar + lastPart;
-      return newString;
     }
-  
-    return finalText;
+    return value;
+  } else {
+    return null;
   }
-  
-  function intoTalic(text) {
-    let box = [];
-    let arr = [];
-    let finalText = "";
-  
-    let a = text.split(" ");
-  
-    let singleStericCounter = 0;
-  
-    a.forEach(e => {
-      if (
-        e.length > 1 &&
-        e.includes("_") &&
-        (e.match(/_/g) || []).length == 1
-      ) {
-        ++singleStericCounter;
-      }
-    });
-    singleStericCounter = singleStericCounter - 1;
-  
-    let flag = "1";
-    let loopCounter = 0;
-    a.forEach(e => {
-      if (
-        e.length > 1 &&
-        e.includes("_") &&
-        (e.match(/_/g) || []).length == 1
-      ) {
-        if (loopCounter == singleStericCounter && loopCounter % 2 == 0) {
-        } else {
-          if (flag == "1") {
-            e = e.replace(/_/g, "<i>");
-            flag = "2";
-          } else {
-            e = e.replace(/_/g, "</i>");
-            flag = "1";
-          }
-          ++loopCounter;
-        }
-      }
-  
-      if (e.length > 1 && (e.match(/_/g) || []).length > 1) {
-        const n = (e.match(/_/g) || []).length;
-  
-        for (let i = 0; i < n; i++) {
-          if (i == 0) {
-            box.push(e.indexOf("_", i));
-          } else {
-            let len = box.length - 1;
-            let v = box[len];
-            v = v + 1;
-            box.push(e.indexOf("_", v));
-          }
-        }
-        let firstIndex = box[0];
-        let lastIndex = box[box.length - 1];
-  
-        e = replaceChar(e, "<i>", firstIndex);
-        e = replaceChar(e, "</i>", lastIndex + 2);
-  
-        box = [];
-      }
-  
-      arr.push(e);
-    });
-  
-    arr.forEach(e => {
-      if (e == "") {
-        finalText = finalText + "";
-      } else {
-        finalText = finalText + e + "";
-      }
-    });
-  
-    function replaceChar(origString, replaceChar, index) {
-      let firstPart = origString.substr(0, index);
-      let lastPart = origString.substr(index + 1);
-  
-      let newString = firstPart + replaceChar + lastPart;
-      return newString;
-    }
-  
-    return finalText;
-  }
-  
-  function intoStrike(text) {
-    let box = [];
-    let arr = [];
-    let finalText = "";
-  
-    let a = text.split(" ");
-  
-    let singleStericCounter = 0;
-  
-    a.forEach(e => {
-      if (
-        e.length > 1 &&
-        e.includes("~") &&
-        (e.match(/~/g) || []).length == 1
-      ) {
-        ++singleStericCounter;
-      }
-    });
-    singleStericCounter = singleStericCounter - 1;
-  
-    let flag = "1";
-    let loopCounter = 0;
-    a.forEach(e => {
-      if (
-        e.length > 1 &&
-        e.includes("~") &&
-        (e.match(/~/g) || []).length == 1
-      ) {
-        if (loopCounter == singleStericCounter && loopCounter % 2 == 0) {
-        } else {
-          if (flag == "1") {
-            e = e.replace(/~/g, "<strike>");
-            flag = "2";
-          } else {
-            e = e.replace(/~/g, "</strike>");
-            flag = "1";
-          }
-          ++loopCounter;
-        }
-      }
-  
-      if (e.length > 1 && (e.match(/~/g) || []).length > 1) {
-        const n = (e.match(/~/g) || []).length;
-  
-        for (let i = 0; i < n; i++) {
-          if (i == 0) {
-            box.push(e.indexOf("~", i));
-          } else {
-            let len = box.length - 1;
-            let v = box[len];
-            v = v + 1;
-            box.push(e.indexOf("~", v));
-          }
-        }
-        let firstIndex = box[0];
-        let lastIndex = box[box.length - 1];
-  
-        e = replaceChar(e, "<strike>", firstIndex);
-        e = replaceChar(e, "</strike>", lastIndex + 7);
-  
-        box = [];
-      }
-  
-      arr.push(e);
-    });
-  
-    arr.forEach(e => {
-      if (e == "") {
-        finalText = finalText + "";
-      } else {
-        finalText = finalText + e + "";
-      }
-    });
-  
-    function replaceChar(origString, replaceChar, index) {
-      let firstPart = origString.substr(0, index);
-      let lastPart = origString.substr(index + 1);
-  
-      let newString = firstPart + replaceChar + lastPart;
-      return newString;
-    }
-  
-    return finalText;
-  }
+}
 
-  module.exports.ibsFormat = ibsFormat;
+function doubleAstericHandler(text, tag, iden, trim, space) {
+  let box = [];
+  let arr = [];
+  let finalText = "";
+  let a = text.split(" ");
+  let singleStericCounter = 0;
+  a.forEach(e => {
+    if (
+      e.length > 1 &&
+      e.includes(iden) &&
+      (e.match(/\*\*/g) || []).length == 1
+    ) {
+      ++singleStericCounter;
+    }
+  });
+  singleStericCounter = singleStericCounter - 1;
+  let flag = "1";
+  let loopCounter = 0;
+  a.forEach(e => {
+    if (
+      e.length > 1 &&
+      e.includes(iden) &&
+      (e.match(/\*\*/g) || []).length == 1
+    ) {
+      if (loopCounter == singleStericCounter && loopCounter % 2 == 0) {
+      } else {
+        if (flag == "1") {
+          e = e.replace(/\*\*/g, "<" + tag + ">");
+          flag = "2";
+        } else {
+          e = e.replace(/\*\*/g, "</" + tag + ">");
+          flag = "1";
+        }
+        ++loopCounter;
+      }
+    }
+    if (e.length > 1 && (e.match(/\*\*/g) || []).length > 1) {
+      const n = (e.match(/\*\*/g) || []).length;
+
+      for (let i = 0; i < n; i++) {
+        if (i == 0) {
+          box.push(e.indexOf(iden, i));
+        } else {
+          let len = box.length - 1;
+          let v = box[len];
+          v = v + 1;
+          box.push(e.indexOf(iden, v));
+        }
+      }
+      e = e.replace('**', "<" + tag + ">");
+      e = e.replace('**', "</" + tag + ">");
+      box = [];
+    }
+    arr.push(e);
+  });
+  arr.forEach(e => {
+    if (e == "") {
+      finalText = finalText + space;
+    } else {
+      finalText = finalText + e + space;
+    }
+  });
+
+  return finalText;
+}
+
+
+function astericHandler(text, tag, iden, trim, space) {
+  let box = [];
+  let arr = [];
+  let finalText = "";
+  let a = text.split(" ");
+  let singleStericCounter = 0;
+  a.forEach(e => {
+    if (
+      e.length > 1 &&
+      e.includes(iden) &&
+      (e.match(/\x2a/g) || []).length == 1
+    ) {
+      ++singleStericCounter;
+    }
+  });
+  singleStericCounter = singleStericCounter - 1;
+  let flag = "1";
+  let loopCounter = 0;
+  a.forEach(e => {
+    if (
+      e.length > 1 &&
+      e.includes(iden) &&
+      (e.match(/\x2a/g) || []).length == 1
+    ) {
+      if (loopCounter == singleStericCounter && loopCounter % 2 == 0) {
+      } else {
+        if (flag == "1") {
+          e = e.replace(/\x2a/g, "<" + tag + ">");
+          flag = "2";
+        } else {
+          e = e.replace(/\x2a/g, "</" + tag + ">");
+          flag = "1";
+        }
+        ++loopCounter;
+      }
+    }
+
+    if (e.length > 1 && (e.match(/\x2a/g) || []).length > 1) {
+      const n = (e.match(/\x2a/g) || []).length;
+
+      for (let i = 0; i < n; i++) {
+        if (i == 0) {
+          box.push(e.indexOf(iden, i));
+        } else {
+          let len = box.length - 1;
+          let v = box[len];
+          v = v + 1;
+          box.push(e.indexOf(iden, v));
+        }
+      }
+      let firstIndex = box[0];
+      let lastIndex = box[box.length - 1];
+      e = replaceChar(e, "<" + tag + ">", firstIndex);
+      e = replaceChar(e, "</" + tag + ">", lastIndex + trim);
+      box = [];
+    }
+
+    arr.push(e);
+  });
+  arr.forEach(e => {
+    if (e == "") {
+      finalText = finalText + space;
+    } else {
+      finalText = finalText + e + space;
+    }
+  });
+  function replaceChar(origString, replaceChar, index) {
+    let firstPart = origString.substr(0, index);
+    let lastPart = origString.substr(index + 1);
+
+    let newString = firstPart + replaceChar + lastPart;
+    return newString;
+  }
+  return finalText;
+}
+
+
+function getFormat(text, tag, iden, trim, space) {
+  let box = [];
+  let arr = [];
+  let finalText = "";
+  let a = text.split(" ");
+  let singleStericCounter = 0;
+  a.forEach(e => {
+    if (
+      e.length > 1 &&
+      e.includes(iden) &&
+      (e.match(new RegExp(iden, "g")) || []).length == 1
+    ) {
+      ++singleStericCounter;
+    }
+  });
+  singleStericCounter = singleStericCounter - 1;
+  let flag = "1";
+  let loopCounter = 0;
+  a.forEach(e => {
+    if (
+      e.length > 1 &&
+      e.includes(iden) &&
+      (e.match(new RegExp(iden, "g")) || []).length == 1
+    ) {
+      if (loopCounter == singleStericCounter && loopCounter % 2 == 0) {
+      } else {
+        if (flag == "1") {
+          e = e.replace(new RegExp(iden, "g"), "<" + tag + ">");
+          flag = "2";
+        } else {
+          e = e.replace(new RegExp(iden, "g"), "</" + tag + ">");
+          flag = "1";
+        }
+        ++loopCounter;
+      }
+    }
+    if (e.length > 1 && (e.match(new RegExp(iden, "g")) || []).length > 1) {
+      const n = (e.match(new RegExp(iden, "g")) || []).length;
+
+      for (let i = 0; i < n; i++) {
+        if (i == 0) {
+          box.push(e.indexOf(iden, i));
+        } else {
+          let len = box.length - 1;
+          let v = box[len];
+          v = v + 1;
+          box.push(e.indexOf(iden, v));
+        }
+      }
+      let firstIndex = box[0];
+      let lastIndex = box[box.length - 1];
+
+      e = replaceChar(e, "<" + tag + ">", firstIndex);
+      e = replaceChar(e, "</" + tag + ">", lastIndex + trim);
+
+      box = [];
+    }
+    arr.push(e);
+  });
+
+  arr.forEach(e => {
+    if (e == "") {
+      finalText = finalText + space;
+    } else {
+      finalText = finalText + e + space;
+    }
+  });
+  function replaceChar(origString, replaceChar, index) {
+    let firstPart = origString.substr(0, index);
+    let lastPart = origString.substr(index + 1);
+
+    let newString = firstPart + replaceChar + lastPart;
+    return newString;
+  }
+  return finalText;
+}
+
+module.exports.ibsFormat = ibsFormat;
